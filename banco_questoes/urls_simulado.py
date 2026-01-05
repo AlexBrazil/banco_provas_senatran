572101db5d5ae2774c6899ba99cac707f90e4f05
Alexandre Bastos (Pessoal) <alexandre@faciltecnologias.com.br>
2026-01-05 14:07:35 -0300

    Add tela de inicio

--- banco_questoes/urls_simulado.py
+++ banco_questoes/urls_simulado.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path("questao/", views_simulado.simulado_questao, name="questao"),
     path("responder/", views_simulado.simulado_responder, name="responder"),
     path("resultado/", views_simulado.simulado_resultado, name="resultado"),
-    #endpoint AJAX
+    # endpoint AJAX
     path("api/modulos/", views_simulado.api_modulos_por_curso, name="api_modulos"),
     path("api/stats/", views_simulado.api_stats, name="api_stats"),
 ]
